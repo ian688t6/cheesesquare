326ce6b03c004a981bed47e92801ffc5081f4623
ian <ian_yin@gcoreinc.com>
2018-03-01 14:51:35 +0800

    update action bar

--- app/src/main/java/gc/com/cheesesquare/MainActivity.java
+++ app/src/main/java/gc/com/cheesesquare/MainActivity.java
@@ -1,7 +1,11 @@
 package gc.com.cheesesquare;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.simple_actions, menu);
+        return true;
     }
 }
