ef06f8a96bb9cc02e414a691561cc1481b79ed16
ian <ian_yin@gcoreinc.com>
2018-03-02 12:12:44 +0800

    add cheese detail

--- app/src/main/java/gc/com/cheesesquare/CheeseDetailActivity.java
+++ app/src/main/java/gc/com/cheesesquare/CheeseDetailActivity.java
@@ -1,14 +1,46 @@
 package gc.com.cheesesquare;
 
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class CheeseDetailActivity extends AppCompatActivity {
+
     public static final String EXTRA_NAME = "cheese_name";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheese_detail);
+
+        Intent intent = getIntent();
+        final String cheeseName = intent.getStringExtra(EXTRA_NAME);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(cheeseName);
+
+        loadBackdrop();
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView =  findViewById(R.id.backdrop);
+        Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.simple_actions, menu);
+        return true;
     }
 }
